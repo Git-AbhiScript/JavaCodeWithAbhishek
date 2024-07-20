332811ba3007cebc985f5a42695851ed0d088a1f
Git-AbhiScript <work.abhishek2080@gmail.com>
2024-07-20 16:53:10 +0530

    first

---
+++ AddThreeNum.java
@@ -0,0 +1,13 @@
+
+    public class AddThreeNum {
+
+        public static void main(String[] args) {
+            System.out.println("Enter 3 numbers");
+            int num1 = 2;
+            int num2 = 3;
+            int num3 = 4;
+            int sum = num1 + num2 + num3;
+            System.out.println(sum);
+        }
+    }
+
