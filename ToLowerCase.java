822b25365ea3d9b189baccf21f36c5517c777e0e
Git-AbhiScript <work.abhishek2080@gmail.com>
2024-07-26 22:01:17 +0530

    LargestFromListOfArray

---
+++ ToLowerCase.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println("Enter the string to convert: ");
+        Scanner sc=new Scanner(System.in);
+        String sc1= sc.next();
+        System.out.println(sc1.toLowerCase());
+    }
+}
